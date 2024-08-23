a2fb674e7a0098081bf02b4209de65e006558be6
ezityoakmar19 <ezityoakmarpradigtia@gmail.com>
2024-08-23 11:27:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+Tidak dapat membuat, mengedit, atau mengupload … Ruang penyimpanan tidak cukup. Dapatkan penyimpanan sebesar 100 GB seharga Rp 26.900,00 Rp 7.000,00/bulan untuk 2 bulan.
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
